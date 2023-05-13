77562d7f62a99fd36f47a90686431cd8412d7160
dwirustanto10 <keizaq100282@gmail.com>
2023-05-13 22:55:51 +0700

    13/5/2023 22.56

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,16 @@
-const { Router } = require("express");
+const { Router } = require('express');
 const route = Router();
 
-route.get("/", (req, res) => {
+route.get('/', (req, res) => {
   res.status(200).json({
-    message: "Inventory Apps",
+    message: 'Academic System Apps',
   });
 });
 
-const userRoutes = require("./user");
-const itemRoutes = require("./item");
+const studentRoutes = require('./student');
+const academicRoutes = require('./academic');
 
-route.use("/users", userRoutes);
-route.use("/items", itemRoutes);
+route.use('/students', studentRoutes);
+route.use('/academics', academicRoutes);
 
 module.exports = route;
